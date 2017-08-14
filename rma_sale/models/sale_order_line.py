04bbc75a725c15f081d03667cf9b814ce9418af2
Andhitia Rama <andhitia.r@gmail.com>
2017-08-14 09:31:44 +0700

    rma_sale

--- rma_sale/models/sale_order_line.py
+++ rma_sale/models/sale_order_line.py
@@ -3,7 +3,7 @@
 # © 2015 Eezee-It, MONK Software, Vauxoo
 # © 2013 Camptocamp
 # © 2009-2013 Akretion,
-# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 from openerp import api, fields, models
 
@@ -11,14 +11,17 @@ from openerp import api, fields, models
 class SaleOrderLine(models.Model):
     _inherit = "sale.order.line"
 
-    rma_line_id = fields.Many2one('rma.order.line', string='RMA',
-                                  ondelete='restrict')
+    rma_line_id = fields.Many2one(
+        comodel_name="rma.order.line",
+        string="RMA",
+        ondelete="restrict",
+    )
 
     @api.multi
     def _prepare_order_line_procurement(self, group_id=False):
         vals = super(SaleOrderLine, self)._prepare_order_line_procurement(
             group_id=group_id)
         vals.update({
-            'rma_line_id': self.rma_line_id.id
+            "rma_line_id": self.rma_line_id.id
         })
         return vals
