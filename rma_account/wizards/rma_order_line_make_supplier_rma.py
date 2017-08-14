cdf3120cc3e2f107277b011907510d5b798bc48d
Andhitia Rama <andhitia.r@gmail.com>
2017-08-14 07:51:00 +0700

    relicense to AGPL-3

--- rma_account/wizards/rma_order_line_make_supplier_rma.py
+++ rma_account/wizards/rma_order_line_make_supplier_rma.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 # Copyright 2016 Eficent Business and IT Consulting Services S.L.
-# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl-3.0).
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 from openerp import api, models
 
