27c8cfaec189f4bbfe350596b39dc1003940caaa
Andhitia Rama <andhitia.r@gmail.com>
2017-08-15 00:37:59 +0700

    improve unittest

--- rma_account/tests/test_rma_customer.py
+++ rma_account/tests/test_rma_customer.py
@@ -24,6 +24,28 @@ class TestRmaCustomer(BaseCase):
             })
         self.obj_wiz_refund = self.env["rma.refund"]
 
+    def _check_quantity(self,
+            rma_line,
+            qty_to_receive=False, qty_incoming=False,
+            qty_received=False, qty_to_deliver=False,
+            qty_outgoing=False, qty_delivered=False,
+            qty_to_supplier_rma=False, qty_in_supplier_rma=False,
+            qty_to_refund=False, qty_refunded=False):
+        super(TestRmaCustomer, self)._check_quantity(
+            rma_line=rma_line,
+            qty_to_receive=qty_to_receive, qty_incoming=qty_incoming,
+            qty_received=qty_received, qty_to_deliver=qty_to_deliver,
+            qty_outgoing=qty_outgoing, qty_delivered=qty_delivered)
+        if not qty_to_refund:
+            self.assertEqual(
+                qty_to_refund,
+                rma_line.qty_to_refund)
+        if not qty_refunded:
+            self.assertEqual(
+                qty_refunded,
+                rma_line.qty_refunded)
+
+
     def test_rma_creation(self):
         line_data = self._prepare_rma_lines(
             self.rar_op)
@@ -35,12 +57,9 @@ class TestRmaCustomer(BaseCase):
         self._rma_request_approval(rma)
         self._rma_approve(rma)
         line = rma.rma_line_ids[0]
-        self.assertEqual(
-            line.qty_to_receive,
-            7.0)
-        self.assertEqual(
-            line.qty_to_refund,
-            0.0)
+        self._check_quantity(line,
+            qty_to_receive=7.0,
+            qty_to_refund=0.0)
         wiz = self.rma_make_picking.with_context({
             "active_model": "rma.order.line",
             "active_ids": [line.id],
@@ -52,25 +71,14 @@ class TestRmaCustomer(BaseCase):
         self.assertEqual(
             len(line.move_ids),
             1)
-        self.assertEqual(
-            line.qty_incoming,
-            3.0)
+        self._check_quantity(line,
+            qty_incoming=3.0)
         self._process_move(line.move_ids[0])
-        self.assertEqual(
-            line.qty_to_receive,
-            4.0)
-        self.assertEqual(
-            line.qty_incoming,
-            0.0)
-        self.assertEqual(
-            line.move_ids[0].product_uom_qty,
-            3.0)
-        self.assertEqual(
-            line.qty_received,
-            3.0)
-        self.assertEqual(
-            line.qty_to_refund,
-            3.0)
+        self._check_quantity(line,
+            qty_to_receive=4.0,
+            qty_received=3.0,
+            qty_to_refund=3.0,
+            qty_incoming=0.0)
         wiz_refund = self.obj_wiz_refund.with_context({
             "active_model": "rma.order.line",
             "active_ids": [line.id],
@@ -78,11 +86,7 @@ class TestRmaCustomer(BaseCase):
         wiz_refund.item_ids[0].qty_to_refund = 2.0
         wiz_refund.invoice_refund()
         line.refund_line_ids.invoice_id.signal_workflow("invoice_open")
-        self.assertEqual(
-            line.qty_to_refund,
-            1.0)
-        self.assertEqual(
-            line.qty_refunded,
-            2.0)
-
+        self._check_quantity(line,
+            qty_to_refund=1.0,
+            qty_refunded=2.0)
         self._rma_done(rma)
