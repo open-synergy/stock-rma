efbb6292dd53a6e0d0c661c61fc6062c08798f85
Andhitia Rama <andhitia.r@gmail.com>
2017-08-15 07:02:46 +0700

    fix travis

--- rma_account/tests/test_rma_customer.py
+++ rma_account/tests/test_rma_customer.py
@@ -6,6 +6,7 @@ from openerp.addons.rma.tests.base import BaseCase
 
 
 class TestRmaCustomer(BaseCase):
+
     def setUp(self):
         super(TestRmaCustomer, self).setUp()
 
@@ -21,11 +22,11 @@ class TestRmaCustomer(BaseCase):
             "in_route_id": self.route_customer.id,
             "out_route_id": self.route_customer.id,
             "location_id": self.stock_rma_location.id,
-            })
+        })
         self.obj_wiz_refund = self.env["rma.refund"]
 
-    def _check_quantity(self,
-            rma_line,
+    def _check_quantity(
+            self, rma_line,
             qty_to_receive=False, qty_incoming=False,
             qty_received=False, qty_to_deliver=False,
             qty_outgoing=False, qty_delivered=False,
@@ -45,7 +46,6 @@ class TestRmaCustomer(BaseCase):
                 qty_refunded,
                 rma_line.qty_refunded)
 
-
     def test_rma_creation(self):
         line_data = self._prepare_rma_lines(
             self.rar_op)
@@ -57,14 +57,15 @@ class TestRmaCustomer(BaseCase):
         self._rma_request_approval(rma)
         self._rma_approve(rma)
         line = rma.rma_line_ids[0]
-        self._check_quantity(line,
+        self._check_quantity(
+            line,
             qty_to_receive=7.0,
             qty_to_refund=0.0)
         wiz = self.rma_make_picking.with_context({
             "active_model": "rma.order.line",
             "active_ids": [line.id],
             "picking_type": "incoming"
-            }).create({})
+        }).create({})
         wiz_item = wiz.item_ids[0]
         wiz_item.qty_to_receive = 3.0
         wiz.action_create_picking()
@@ -72,9 +73,10 @@ class TestRmaCustomer(BaseCase):
             len(line.move_ids),
             1)
         self._check_quantity(line,
-            qty_incoming=3.0)
+                             qty_incoming=3.0)
         self._process_move(line.move_ids[0])
-        self._check_quantity(line,
+        self._check_quantity(
+            line,
             qty_to_receive=4.0,
             qty_received=3.0,
             qty_to_refund=3.0,
@@ -82,11 +84,12 @@ class TestRmaCustomer(BaseCase):
         wiz_refund = self.obj_wiz_refund.with_context({
             "active_model": "rma.order.line",
             "active_ids": [line.id],
-            }).create({})
+        }).create({})
         wiz_refund.item_ids[0].qty_to_refund = 2.0
         wiz_refund.invoice_refund()
         line.refund_line_ids.invoice_id.signal_workflow("invoice_open")
-        self._check_quantity(line,
+        self._check_quantity(
+            line,
             qty_to_refund=1.0,
             qty_refunded=2.0)
         self._rma_done(rma)
