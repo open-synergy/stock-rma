208a5aec526369d0f1c0054743fcf6362136a571
Andhitia Rama <andhitia.r@gmail.com>
2017-08-15 17:59:55 +0700

    [rma_sale] change api.one into api.multi

--- rma_sale/models/rma_order.py
+++ rma_sale/models/rma_order.py
@@ -10,19 +10,29 @@ from openerp import api, fields, models
 class RmaOrder(models.Model):
     _inherit = "rma.order"
 
-    @api.one
+    @api.multi
+    @api.depends(
+        "rma_line_ids",
+        "rma_line_ids.sale_line_id",
+    )
     def _compute_sales_count(self):
-        sales_list = []
-        for rma_line in self.rma_line_ids:
-            if rma_line.sale_line_id and rma_line.sale_line_id.id:
-                sales_list.append(rma_line.sale_line_id.order_id.id)
-        self.sale_count = len(list(set(sales_list)))
+        for rma in self:
+            sales_list = []
+            for rma_line in rma.rma_line_ids:
+                if rma_line.sale_line_id and rma_line.sale_line_id.id:
+                    sales_list.append(rma_line.sale_line_id.order_id.id)
+            rma.sale_count = len(list(set(sales_list)))
 
-    sale_count = fields.Integer(compute=_compute_sales_count,
-                                string="# of Sales", copy=False, default=0)
+    sale_count = fields.Integer(
+        compute=_compute_sales_count,
+        string="# of Sales",
+        copy=False,
+        default=0,
+    )
 
     @api.model
     def _get_line_domain(self, rma_id, line):
+        # TODO: Review
         if line.sale_line_id and line.sale_line_id.id:
             domain = [("rma_id", "=", rma_id.id),
                       ("type", "=", "supplier"),
@@ -33,6 +43,7 @@ class RmaOrder(models.Model):
 
     @api.multi
     def action_view_sale_order(self):
+        # TODO: Review
         action = self.env.ref("sale.action_quotations")
         result = action.read()[0]
         order_ids = []
