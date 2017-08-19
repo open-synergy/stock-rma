5a00b6ac3a3c238603942fc40b9b2178a1f7a88d
Andhitia Rama <andhitia.r@gmail.com>
2017-08-19 18:14:26 +0700

    [rma] more unittest. Fix receipt_policy selection on rma.operation

--- rma/tests/__init__.py
+++ rma/tests/__init__.py
@@ -4,4 +4,4 @@
 # © 2013 Camptocamp
 # © 2009-2013 Akretion,
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
-from . import test_rma_customer_00
+from . import test_rma_customer_00, test_rma_customer_01
