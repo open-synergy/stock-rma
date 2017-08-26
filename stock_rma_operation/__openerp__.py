8a1ef30745dea6e4e11848d64782bb89b6100bc7
Andhitia Rama <andhitia.r@gmail.com>
2017-08-26 22:23:51 +0700

    [8.0.1.1.0] stock_rma_location
    * Add create RMA location button

--- stock_rma_operation/__openerp__.py
+++ stock_rma_operation/__openerp__.py
@@ -3,7 +3,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
     "name": "RMA Picking Operation",
-    "version": "8.0.1.0.0",
+    "version": "8.0.1.1.0",
     "author": "OpenSynergy Indonesia",
     "website": "https://opensynergy-indonesia.com",
     "category": "Stock Management",
