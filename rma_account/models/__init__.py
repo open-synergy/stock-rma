efb879da3651d840986121df4835a3885fccbc56
Andhitia Rama <andhitia.r@gmail.com>
2018-01-10 23:05:54 +0700

    Change into new qty computation format

--- rma_account/models/__init__.py
+++ rma_account/models/__init__.py
@@ -1,9 +1,11 @@
 # -*- coding: utf-8 -*-
+# Copyright 2018 OpenSynergy Indonesia
 # © 2017 Eficent Business and IT Consulting Services S.L.
 # © 2015 Eezee-It, MONK Software, Vauxoo
 # © 2013 Camptocamp
 # © 2009-2013 Akretion,
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+from . import rma_policy
 from . import rma_order
 from . import rma_order_line
 from . import rma_operation
