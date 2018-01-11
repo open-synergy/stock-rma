d6f3773e7a3a68365bdc72e1f6626146370bbf98
Andhitia Rama <andhitia.r@gmail.com>
2018-01-11 17:27:47 +0700

    [rma] improve test

--- rma/tests/test_rma_customer_00.py
+++ rma/tests/test_rma_customer_00.py
@@ -3,6 +3,8 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 from .base import BaseCase
+import csv
+import os
 
 
 class TestRmaCustomer(BaseCase):
@@ -11,12 +13,14 @@ class TestRmaCustomer(BaseCase):
         super(TestRmaCustomer, self).setUp()
 
         self.cust_op = self.rma_op.create({
-            "name": "Replace After Receive",
-            "code": "RAR",
+            "name": "Test RMA Operation",
+            "code": "TEST",
             "receipt_policy_id": self.env.\
-                ref("rma.rma_policy_ordered_received").id,
+                ref("rma.rma_policy_no").id,
             "delivery_policy_id": self.env.\
-                ref("rma.rma_policy_received_delivered").id,
+                ref("rma.rma_policy_no").id,
+            "rma_supplier_policy_id": self.env.\
+                ref("rma.rma_policy_no").id,
             "type": "customer",
             "in_warehouse_id": self.wh.id,
             "out_warehouse_id": self.wh.id,
@@ -26,6 +30,7 @@ class TestRmaCustomer(BaseCase):
         })
 
     def test_customer_rma(self):
+        # Create and Approve RMA
         rma = self._create_rma(
             operation=self.cust_op,
             rma_type="customer",
@@ -33,99 +38,53 @@ class TestRmaCustomer(BaseCase):
         self._rma_request_approval(rma)
         self._rma_approve(rma)
         line = rma.rma_line_ids[0]
-        self._check_quantity(
-            line,
-            qty_to_receive=7.0,
-            qty_received=0.0,
-            qty_incoming=0.0,
-            qty_to_deliver=0.0,
-            qty_delivered=0.0,
-            qty_outgoing=0.0,
-        )
+
+        # Create Incoming Picking
         wiz = self.rma_make_picking.with_context({
             "active_model": "rma.order.line",
             "active_ids": [line.id],
             "picking_type": "incoming"
         }).create({})
         wiz_item = wiz.item_ids[0]
-        wiz_item.qty_to_receive = 3.0
+        wiz_item.qty_to_receive = 5.0
         wiz.action_create_picking()
-        self._check_quantity(
-            line,
-            qty_to_receive=7.0,
-            qty_received=0.0,
-            qty_incoming=3.0,
-            qty_to_deliver=0.0,
-            qty_delivered=0.0,
-            qty_outgoing=0.0,
-        )
         self._process_move(line.move_ids)
         self._check_quantity(
             line,
-            qty_to_receive=4.0,
-            qty_received=3.0,
-            qty_incoming=0.0,
-            qty_to_deliver=3.0,
-            qty_delivered=0.0,
-            qty_outgoing=0.0,
-        )
+            qty_received=5.0
+            )
+        self._run_qty_check(line, "rma_customer_case_01.csv")
+
+        # Create Outgoing Picking
         wiz_out_1 = self.rma_make_picking.with_context({
             "active_model": "rma.order.line",
             "active_ids": [line.id],
             "picking_type": "outgoing"
         }).create({})
         wiz_item = wiz_out_1.item_ids[0]
-        wiz_item.qty_to_deliver = 2.0
+        wiz_item.qty_to_deliver = 3.0
         wiz_out_1.action_create_picking()
-        self._check_quantity(
-            line,
-            qty_to_receive=4.0,
-            qty_received=3.0,
-            qty_incoming=0.0,
-            qty_to_deliver=3.0,
-            qty_delivered=0.0,
-            qty_outgoing=2.0,
-        )
         self._process_move(line.move_ids)
         self._check_quantity(
             line,
-            qty_to_receive=4.0,
-            qty_received=3.0,
-            qty_incoming=0.0,
-            qty_to_deliver=1.0,
-            qty_delivered=2.0,
-            qty_outgoing=0.0,
-        )
-        wiz_out_2 = self.rma_make_picking.with_context({
+            qty_delivered=3.0
+            )
+
+        # Make supplier RMA
+        wiz = self.make_supplier_rma.with_context({
             "active_model": "rma.order.line",
             "active_ids": [line.id],
-            "picking_type": "outgoing"
         }).create({})
-        wiz_item = wiz_out_2.item_ids[0]
-        wiz_item.qty_to_deliver = 1.0
-        wiz_out_2.action_create_picking()
-        self.assertEqual(
-            len(line.move_ids),
-            3)
+        wiz.item_ids[0].product_qty = 2.0
+        wiz.make_supplier_rma()
         self._check_quantity(
             line,
-            qty_to_receive=4.0,
-            qty_received=3.0,
-            qty_incoming=0.0,
-            qty_to_deliver=1.0,
-            qty_delivered=2.0,
-            qty_outgoing=1.0,
-        )
-        self._process_move(line.move_ids)
-        self._check_quantity(
-            line,
-            qty_to_receive=4.0,
-            qty_received=3.0,
-            qty_incoming=0.0,
-            qty_to_deliver=0.0,
-            qty_delivered=3.0,
-            qty_outgoing=0.0,
-        )
+            qty_in_supplier_rma=2.0
+            )
+
+        self._run_qty_check(line, "rma_customer_case_99.csv")
+                    
+        # Done RMA
         self._rma_done(rma)
 
         # Line's action
@@ -138,15 +97,3 @@ class TestRmaCustomer(BaseCase):
         rma.action_view_supplier_lines()
         rma.action_view_in_shipments()
         rma.action_view_out_shipments()
-
-        # Make supplier RMA
-        wiz = self.make_supplier_rma.with_context({
-            "active_model": "rma.order.line",
-            "active_ids": [line.id],
-        }).create({})
-        wiz.item_ids[0].product_qty = 3.0
-        wiz.make_supplier_rma()
-        self._check_quantity(
-            line,
-            qty_in_supplier_rma=3.0,
-            qty_to_supplier_rma=0.0)
