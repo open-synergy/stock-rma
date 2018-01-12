1d315245ab86f96ea4f2bb53182953c20cbbafeb
Andhitia Rama <andhitia.r@gmail.com>
2018-01-12 16:04:40 +0700

    [rma] Add exception handler for policies default method

--- rma/models/__init__.py
+++ rma/models/__init__.py
@@ -5,12 +5,12 @@
 # © 2009-2013 Akretion,
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from . import rma_order
-from . import rma_order_line
-from . import rma_operation
 from . import rma_policy_field
 from . import rma_policy
 from . import rma_policy_rule
+from . import rma_order
+from . import rma_order_line
+from . import rma_operation
 from . import stock
 from . import stock_warehouse
 from . import product
